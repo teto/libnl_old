cc576ef298c741ad5936b49aa4e4999a7c6a19f9
Matt <mattator@gmail.com>
2013-10-21 11:58:39 +0200

    Started rule binding

--- python/netlink/route/rule.py
+++ python/netlink/route/rule.py
@@ -8,10 +8,25 @@
 from __future__ import absolute_import
 
 
+import netlink.core as netlink
 # __version__ = '1.0'
 # __all__ = [
 # ]
 
+# cacheable
+class Rule(netlink.Object):
+
+    def __init__(self, obj=None):
+        # pass 
+        # self._nl_route = capir.rtnl_route_alloc()
+
+        super().__init__( "route/rule", "rule", obj )
+
+        self._nl_rule = self._obj2type(self._nl_object)
+
+    def _obj2type(self):
+        raise NotImplementedError();
+        
 
 # class RoutingTableCache(netlink.Cache):
 # 	def __init__(self):
